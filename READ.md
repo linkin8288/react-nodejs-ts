84444dc55a35c79c215a962fb26a45d2130ecd9b
charlie <linkin8288@gmail.com>
2023-08-11 16:23:51 +0200

    Connect Mongodb but with connect bug

--- READ.md
+++ READ.md
@@ -5,3 +5,7 @@
 3. @types/express is a package that provides TypeScript type definitions for the Express.js framework.
 
 4. Nodemon-watch is a utility that automatically restarts your Node.js application whenever changes are detected.
+
+5. install mongoose and connect the mongodb database.
+
+6. app.use(express.json()); this line of code is used in an Express.js application to enable the middleware that parses incoming JSON data from HTTP requests.
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,31 @@
 import express, { Request, Response } from "express";
+import mongoose from "mongoose";
+import Deck from './models/Deck';
 
-const app = express();
 const PORT = 5000;
+const app = express();
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("hello node")
+// the middleware that parses incoming JSON data from HTTP requests.
+app.use(express.json());
+
+app.post("/decks", async (req: Request, res: Response) => {
+  // initiate an object from Deck class
+  const newDeck = new Deck({
+    title: req.body.title
+  });
+  const createdDeck = await newDeck.save();
+  res.json(createdDeck);
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is listening on port ${PORT}`);
+app.get("/decks", (req: Request, res: Response) => {
+  // initiate an object from Deck class
+  res.send("Hello World")
 });
+
+mongoose
+  .connect(
+    'mongodb+srv://admin:admin@cluster0.gtcqij2.mongodb.net/?retryWrites=true&w=majority')
+  .then(() => {
+    console.log(`app is listening on port ${PORT}`);
+    app.listen(PORT);
+  });
